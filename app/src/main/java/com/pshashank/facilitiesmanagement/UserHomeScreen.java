e24c05c63a3da2d96c4ad7595ae5b81f1f6aacfa
TW4R4K4V1 <venkatasaimanoj.twarakavi@mavs.uta.edu>
2019-09-23 22:40:56 -0500

    User Home Screen

---
+++ app/src/main/java/com/pshashank/facilitiesmanagement/UserHomeScreen.java
@@ -0,0 +1,14 @@
+package com.pshashank.facilitiesmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserHomeScreen extends LoginActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_screen2);
+    }
+}
