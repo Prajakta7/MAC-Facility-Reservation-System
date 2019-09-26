b6450b1648103a39bb8c911f41c17d5830d8b6c1
pShashank <pshashank88@gmail.com>
2019-09-25 23:41:54 -0500

    Logout Functionality, Layout change

--- app/src/main/java/com/pshashank/facilitiesmanagement/FMActivity.java
+++ app/src/main/java/com/pshashank/facilitiesmanagement/FMActivity.java
@@ -1,8 +1,13 @@
 package com.pshashank.facilitiesmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class FMActivity extends LoginActivity {
 
@@ -10,5 +15,33 @@ public class FMActivity extends LoginActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fm);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_main_menu,menu);
+
+        if(menu instanceof MenuBuilder){
+
+            MenuBuilder menuBuilder = (MenuBuilder) menu;
+            menuBuilder.setOptionalIconsVisible(true);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout:
+//                Toast.makeText(getApplicationContext(),"Add Clicked",Toast.LENGTH_LONG).show();
+                Intent in = new Intent(FMActivity.this, LoginActivity.class);
+                startActivity(in);
+                return true;
+        }
+        return true;
     }
 }
